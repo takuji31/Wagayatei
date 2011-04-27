42eab660c0899c2116a0216327bf1e33471fdd2c
Nishibayashi Takuji <takuji@senchan.jp>
2011-04-27 20:22:42 +0900

    最初にユーザーがあるか見る

--- lib/Wagayatei/Web/Context.pm
+++ lib/Wagayatei/Web/Context.pm
@@ -3,5 +3,10 @@ use Chiffon::Core;
 use Wagayatei::Container;
 use parent qw/ Chiffon::Web::Context /;
 
+sub user {
+    my $self = shift;
+    $self->stash->{user};
+}
+
 1;
 
--- lib/Wagayatei/Web/Controller.pm
+++ lib/Wagayatei/Web/Controller.pm
@@ -2,6 +2,7 @@ package  Wagayatei::Web::Controller;
 use Chiffon::Core;
 use parent qw/ Chiffon::Web::Controller /;
 use Wagayatei::Container;
+use Wagayatei::DB;
 
 
 __PACKAGE__->add_trigger(
@@ -9,6 +10,12 @@ __PACKAGE__->add_trigger(
         my ( $class, $c ) = @_;
         $c->stash->{site_title} = container('label')->{guild_name};
         $c->stash->{page_title} = $c->dispatch_rule->{action} . " | " . $c->dispatch_rule->{controller};
+        my $db = Wagayatei::DB->get_db;
+        my $user_id = $c->session->get('user_id');
+        if ( defined $user_id ) {
+            my $user = $db->single('user',{ id => $user_id });
+            $c->stash->{user} = $user;
+        }
     }, 
 
 );
