1ea73c0bbc0dd15006e5302366858f48da383471
Nishibayashi Takuji <takuji@senchan.jp>
2011-06-07 02:09:44 +0900

    これはWeb.pmにあるのが正解だと思う

--- lib/Wagayatei/Web.pm
+++ lib/Wagayatei/Web.pm
@@ -5,11 +5,29 @@ use warnings;
 use parent qw/ Wagayatei Chiffon::Web /;
 
 use Chiffon::Plugin::Web::Session;
+use Wagayatei::Container;
+use Wagayatei::DB;
+
 
 __PACKAGE__->set_use_modules(
     request  => 'Wagayatei::Web::Request',
     response => 'Wagayatei::Web::Response',
     router   => 'Wagayatei::Web::Router',
+);
+
+__PACKAGE__->add_trigger(
+    before_action => sub{
+        my $self = shift;
+        $self->stash->{site_title} = container('label')->{guild_name};
+        $self->stash->{page_title} = $self->action . " | " . $self->controller;
+        my $db = Wagayatei::DB->get_db;
+        my $user_id = $self->session->get('user_id');
+        if ( defined $user_id ) {
+            my $user = $db->single('user',{ id => $user_id });
+            $self->stash->{user} = $user;
+        }
+    }, 
+
 );
 
 sub user {shift->stash->{user}}
--- lib/Wagayatei/Web/Controller.pm
+++ lib/Wagayatei/Web/Controller.pm
@@ -3,25 +3,6 @@ use strict;
 use warnings;
 
 use parent qw/ Chiffon::Web::Controller /;
-use Wagayatei::Container;
-use Wagayatei::DB;
-
-
-__PACKAGE__->add_trigger(
-    before_action => sub{
-        my ( $class, $c ) = @_;
-        $c->stash->{site_title} = container('label')->{guild_name};
-        $c->stash->{page_title} = $c->action . " | " . $c->controller;
-        my $db = Wagayatei::DB->get_db;
-        my $user_id = $c->session->get('user_id');
-        if ( defined $user_id ) {
-            my $user = $db->single('user',{ id => $user_id });
-            $c->stash->{user} = $user;
-        }
-    }, 
-
-);
-
 use Data::GUID;
 
 sub create_uuid {
